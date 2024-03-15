2c3f335d47221f94b99787b5a9e599aa5019f7cf
yolan-at-hsl <yolan@hemisynclab.eu>
2024-03-15 13:32:34 +0100

    installed react95

--- front-tier/bios/next.config.mjs
+++ front-tier/bios/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        // Add file-loader for font files
+        config.module.rules.push({
+            test: /\.(woff2|woff|ttf|otf|eot)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    publicPath: '/_next/static/fonts/',
+                    outputPath: `${isServer ? '../' : ''}static/fonts/`,
+                    name: '[name]-[hash].[ext]',
+                },
+            },
+        });
+
+        return config;
+    }
+};
 
 export default nextConfig;
