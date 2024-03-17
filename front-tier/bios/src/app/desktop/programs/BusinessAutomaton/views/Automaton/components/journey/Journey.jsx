00ae927ce8dc37d1ff1653bd8484d7a694097ca1
yolan-at-hsl <yolan@hemisynclab.eu>
2024-03-17 18:04:33 +0100

    Start Journey

---
+++ front-tier/bios/src/app/desktop/programs/BusinessAutomaton/views/Automaton/components/journey/Journey.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+// Business Life Cycle Stages
+// Existence: Focus on establishing the business, initial products or services, and securing the first customers.
+// Survival: Concentrate on sustaining operations, managing finances, and achieving a stable customer base.
+// Success: Expand business operations, diversify products/services, and increase market share.
+// Take-off: Scale the business rapidly, seek significant funding, and possibly go public.
+// Resource Maturity: Achieve stable growth, innovate within the industry, and explore new markets or acquisitions.
+// Gameplay Mechanics
+
+// Players make decisions affecting various aspects of the business, such as hiring, product development, marketing, and partnerships.
+// The outcome of these decisions impacts the business's progress through the life cycle stages.
+
+// Dynamic Events
+// Introduce random events that can positively or negatively affect the business, adding unpredictability and realism to the gameplay.
+
+export default function Journey(activeBusiness) {
+    
+    var { activeBusiness } = activeBusiness;
+
+    // Go through each business life cycle stage
+    // Start with the mission statement
+    // for a stage generate 5 episodes
+    // each episode has a decision and a result
+    console.log(activeBusiness);
+
+    return (
+        <>
+
+        </>
+    )
+}
